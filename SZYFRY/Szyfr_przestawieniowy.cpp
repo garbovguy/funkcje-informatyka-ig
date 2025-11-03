0356b166b466d14d30f5d75d5d438a4abdddb0dd
JustSomeone <73115398+garbovguy@users.noreply.github.com>
2025-11-03 19:31:59 +0100

    SZYFR PRZESTAWIENIOWY

---
+++ SZYFRY/Szyfr_przestawieniowy.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+string przestaw(string tj, int kl){
+string szyfrogram="";
+int i, j;
+while(tj.size()%kl!=0) tj=tj+'X';
+for(j = 0; j<kl;j++)
+    for(i = 0; i<tj.size()/kl; i++)
+    szyfrogram+=tj[i*kl+j];
+return szyfrogram;
+}
+
+int main(){
+string tj;
+int kl;
+cout<<"Podaj tekst: ";
+getline(cin, tj);
+cout<<"podaj klucz: ";
+cin>>kl;
+cout<<przestaw(tj, kl);
+}
